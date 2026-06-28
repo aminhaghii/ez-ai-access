8aa2dd85776e65963a3ad78b767585294029f3fa
Yazdan Fathali <yazduntube@gmail.com>
2026-06-28 10:13:42 +0330

    update open graph

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -7,9 +7,11 @@ export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
 export default async function Image() {
-  const fontData = await readFile(
-    join(process.cwd(), "app/fonts/IRANYekanXFaNum-Bold.woff"),
+  const imageData = await readFile(
+    join(process.cwd(), "app/assets/pfp.jpg"),
+    { encoding: "base64" },
   );
+  const imageSrc = `data:image/jpeg;base64,${imageData}`;
 
   return new ImageResponse(
     <div
@@ -17,45 +19,21 @@ export default async function Image() {
         width: "100%",
         height: "100%",
         background: "#000",
-        color: "#fff",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        padding: "80px",
       }}
     >
-      <div
+      <img
+        src={imageSrc}
+        width={320}
+        height={320}
         style={{
-          fontSize: 80,
-          fontWeight: 700,
-          letterSpacing: "-0.02em",
+          borderRadius: "50%",
+          objectFit: "cover",
         }}
-      >
-        دسترسی رایگان به هوش مصنوعی
-      </div>
-      <div
-        style={{
-          fontSize: 28,
-          color: "#a1a1aa",
-          marginTop: 24,
-          textAlign: "center",
-        }}
-      >
-        بچهای کامیونیتی اینور اونور API های رایگان هوش مصنوعی رو میذارن. فعلا
-        اینجارو برای دسترسی ساده تر درست کردم و منابع خوبی رو که ببینم میذارم.
-      </div>
+      />
     </div>,
-    {
-      ...size,
-      fonts: [
-        {
-          name: "IRANYekanX",
-          data: fontData,
-          style: "normal",
-          weight: 700,
-        },
-      ],
-    },
+    { ...size },
   );
 }
